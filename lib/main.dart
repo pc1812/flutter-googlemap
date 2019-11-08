3a4e192d5d76aa3185a31a2c8c75b531efba8d1b
Paul <chu.nap.blog@gmail.com>
2019-11-08 10:53:58 +0800

    add geolocation.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'dart:async';
+import 'package:geolocator/geolocator.dart';
 
 void main() => runApp(MyApp());
 
@@ -20,7 +22,8 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      // home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: GeolocationExample(),
     );
   }
 }
@@ -109,3 +112,68 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 }
+
+class GeolocationExample extends StatefulWidget {
+  @override
+  GeolocationExampleState createState() => new GeolocationExampleState();
+}
+
+
+class GeolocationExampleState extends State {
+  Geolocator _geolocator;
+  Position _position;
+  
+  void checkPermission() {
+    _geolocator.checkGeolocationPermissionStatus().then((status) { print('status: $status'); });
+    _geolocator.checkGeolocationPermissionStatus(locationPermission: GeolocationPermission.locationAlways).then((status) { print('always status: $status'); });
+    _geolocator.checkGeolocationPermissionStatus(locationPermission: GeolocationPermission.locationWhenInUse)..then((status) { print('whenInUse status: $status'); });
+  }
+  
+  @override
+  void initState() {
+    super.initState();
+  
+    _geolocator = Geolocator();
+    LocationOptions locationOptions = LocationOptions(accuracy: LocationAccuracy.high, distanceFilter: 0);
+  
+    checkPermission();
+    // updateLocation();
+  
+    Stream<Position> stream = _geolocator.getPositionStream(locationOptions);
+    StreamSubscription<Position> streamSub = stream.listen(
+      (Position position) {
+        _position = position;
+      })
+    ;
+  }
+  
+  void updateLocation() async {
+    try {
+      Position newPosition = await Geolocator()
+        .getCurrentPosition(desiredAccuracy: LocationAccuracy.high)
+        .timeout(new Duration(seconds: 5));
+  
+      setState(() {
+        _position = newPosition;
+      });
+    } catch (e) {
+      print('Error: ${e.toString()}');
+    }
+  }
+  
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Startup Name Generator'),
+      ),
+      body: Center(
+        child: Text(
+          'Latitude: ${_position != null ? _position.latitude.toString() : '0'},'
+          ' Longitude: ${_position != null ? _position.longitude.toString() : '0'}'
+        )
+      ),
+    );
+  }
+}
+  
